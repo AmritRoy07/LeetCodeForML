0b2ae0a9efabcf95983e04897505895f8f4311ef
aayu-sha <130537872+aayu-sha@users.noreply.github.com>
2024-05-24 11:04:13 +0530

    53

--- Solutions/Array/53.py
+++ Solutions/Array/53.py
@@ -1,16 +1,11 @@
-import sys
-
 class Solution:
-    def maxSubArray(self, nums: [-2,1,-3,4,-1,2,1,-5,4]) -> int:
-        n=len(nums)
-        maxSum=-sys.maxsize
-        for i in range(n):
-            sum=0
-            for j in range(i,n):
-                sum +=nums[j]
-                maxSum=max(maxSum,sum)
-
+    def maxSubArray(self, nums: list[int]) -> int:
+        maxSum = nums[0] 
+        currentSum = nums[0]  
+        n = len(nums)
+        
+        for i in range(1, n): 
+            currentSum = max(nums[i], currentSum + nums[i])  
+            maxSum = max(maxSum, currentSum)  
+        
         return maxSum
-
-n1=[-2,1,-3,4,-1,2,1,-5,4]
-print(Solution())
